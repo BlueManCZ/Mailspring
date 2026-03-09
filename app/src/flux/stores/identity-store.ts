6ff13c93a1963b58a19422efa0ec299272557256
Ben Gotow <ben@foundry376.com>
2026-03-09 16:29:26 -0500

    Don’t run logout (which uses secret storage) in github action tests

--- app/src/flux/stores/identity-store.ts
+++ app/src/flux/stores/identity-store.ts
@@ -151,13 +151,13 @@ class _IdentityStore extends MailspringStore {
   };
 
   _onLogoutMailspringIdentity = async () => {
+    // Do not touch the keychain or restart the app during specs.
+    if (AppEnv.inSpecMode()) return;
     await this.saveIdentity(null);
     // We need to relaunch the app to clear the webview session
     // and prevent the webview from re signing in with the same MailspringID
-    if (!AppEnv.inSpecMode()) {
-      require('@electron/remote').app.relaunch();
-      require('@electron/remote').app.quit();
-    }
+    require('@electron/remote').app.relaunch();
+    require('@electron/remote').app.quit();
   };
 
   /**
